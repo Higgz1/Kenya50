b3ca6132bcb32a9ddb3a5cb50dfb8631445d4bb9
Bryan Higgz <bryan.kahiga@gmail.com>
2022-03-21 14:04:36 +0300

    recoil init

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { RecoilRoot } from "recoil";
 import Actual from "./Actual";
 
 export default function App() {
@@ -11,10 +12,12 @@ export default function App() {
     //   <Actual/>
     // </View>
     <NativeBaseProvider>
+      <RecoilRoot>
       <SafeAreaView style={styles.container}>
         <StatusBar style="auto" />
         <Actual />
       </SafeAreaView>
+      </RecoilRoot>
     </NativeBaseProvider>
   );
 }
