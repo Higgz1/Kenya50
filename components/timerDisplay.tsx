cc2e813a18b2fa42c744aa24c9622986c587a752
Bryan Higgz <bryan.kahiga@gmail.com>
2022-04-08 16:54:43 +0300

    start of timer logic

--- components/timerDisplay.tsx
+++ components/timerDisplay.tsx
@@ -2,22 +2,30 @@ import { StyleSheet, View } from "react-native";
 import { Text } from "native-base";
 import React from "react";
 import { useRecoilState } from "recoil";
-import { centiState, secondState } from "../atoms/timerAtom";
+import { centiState, secondState, timerState } from "../atoms/timerAtom";
+import moment from "moment";
 
 const TimerDisplay = () => {
-  
   const [secondTime, setSecondTime] = useRecoilState(secondState);
   const [centiSecondTime, setmicroSecondTime] = useRecoilState(centiState);
+  const [timeState, setTimeState] = useRecoilState(timerState);
 
+  const startTimer = () => {
+    const now = moment().format("h:mm:ss a");
+    const then = moment().add(50, 's').format("h:mm:ss a");
+    console.log({ then });
+  };
+
+  if (timeState === true) {
+    console.log("start timer");
+    startTimer();
+  } else {
+    console.log("stop timer");
+  }
 
   return (
     <View style={styles.container}>
-      <Text
-        fontSize="6xl"
-        fontWeight="400"
-        color="darkText"
-        marginTop="8"
-      >
+      <Text fontSize="6xl" fontWeight="400" color="darkText" marginTop="8">
         {secondTime}:{centiSecondTime}
       </Text>
     </View>
@@ -27,8 +35,8 @@ const TimerDisplay = () => {
 export default TimerDisplay;
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     alignItems: "center",
     justifyContent: "center",
-  }
+  },
 });
