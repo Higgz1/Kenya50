113d53a811f339da187ffeb7e4b045ac6745b1f4
Bryan Higgz <bryan.kahiga@gmail.com>
2022-04-08 15:54:09 +0300

    timerButton and atoms set

--- atoms/timerAtom.tsx
+++ atoms/timerAtom.tsx
@@ -9,3 +9,8 @@ export const centiState = atom({
   key: "centiState",
   default: "0" + 0,
 });
+
+export const timerState = atom({
+  key:"timerState",
+  default:false
+})
--- components/timerButtons.tsx
+++ components/timerButtons.tsx
@@ -2,62 +2,66 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Box, Button, Icon, IconButton } from "native-base";
 import { Entypo } from "@expo/vector-icons";
+import { useRecoilState } from "recoil";
+import { timerState } from "../atoms/timerAtom";
 
-const controlls = [{ id: 1, icon: "controller-play" }, {}, {}];
+const controlls = [
+  { id: 1, icon: "controller-play" },
+  { id: 2, icon: "controller-play" },
+  {},
+];
 
 const TimerButtons = () => {
+  const [timeState, setTimeState] = useRecoilState(timerState);
+
+  const Play = () => {
+    setTimeState(!timeState);
+    console.log({ timeState });
+  };
+
+  const reset=()=>{
+    setTimeState(false);
+    console.log({ timeState });
+
+
+  }
+
   return (
-    <View style={[styles.container]}>
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="space-between"
-        alignItems="center"
-        style={styles.controller}
+    <Box
+      display="flex"
+      flexDirection="row"
+      justifyContent="center"
+      alignItems="center"
+      style={styles.controller}
+    >
+      <Button
+        h={24}
+        w={24}
+        borderRadius="full"
+        onPress={Play}
+        style={styles.controller__Playbtn}
       >
-        <Button
-          h={16}
-          w={16}
-          mx={8}
-          borderRadius="full"
-          style={styles.controller__Playbtn}
-        >
-          Hi
-        </Button>
-
-        <Button
-          h={24}
-          w={24}
-          borderRadius="full"
-          style={styles.controller__Playbtn}
-        >
+        {timeState ? (
+          <Entypo name="controller-paus" size={48} color="white" />
+        ) : (
           <Entypo name="controller-play" size={48} color="white" />
-        </Button>
-
-        {/* <Button
-          h={16}
-          w={16}
-          mx={8}
-          borderRadius="full"
-          style={styles.controller__Playbtn}
-        >
-          <Entypo name="share-alternative" size={24} color="black" />
-        </Button> */}
-
-        <IconButton
-          size="lg"
-          borderRadius="full"
-          mx={8}
-
-          // colorScheme="indigo"
-          variant="outline"
-          _icon={{
-            as: Entypo,
-            name: "ccw",
-          }}
-        />
-      </Box>
-    </View>
+        )}
+      </Button>
+      
+      <IconButton
+        size="lg"
+        borderRadius="full"
+        mx={8}
+        key={1}
+        onPress={reset}
+        // colorScheme="indigo"
+        variant="outline"
+        _icon={{
+          as: Entypo,
+          name: "ccw",
+        }}
+      />
+    </Box>
   );
 };
 
